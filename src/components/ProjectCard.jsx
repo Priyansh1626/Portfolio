db013783f37a5d8c6966d66db4381c372e3f5b97
Priyansh <priyanshpandey2055@gmail.com>
2022-09-28 20:00:29 +0530

    added active class

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -5,19 +5,17 @@ export default function ProjectCard(prop) {
     <>
       <div className="col-md-4 p-0">
         <div className="projectCard">
-          <div>
-            <img src={prop.img} alt="" />
-            <div className="hoverDivBox">
-              <div className="top">{prop.top}</div>
-              <div className="bottom">{prop.bottom}</div>
-            </div>
-            <div className="hoverButtonBox">
-              <button>
-                <a href={prop.btnLink} target="_black">
-                  {prop.btn}
-                </a>
-              </button>
-            </div>
+          <img src={prop.img} alt="" />
+          <div className="hoverDivBox">
+            <div className="top">{prop.top}</div>
+            <div className="bottom">{prop.bottom}</div>
+          </div>
+          <div className="hoverButtonBox">
+            <button>
+              <a href={prop.btnLink} target="_black">
+                {prop.btn}
+              </a>
+            </button>
           </div>
         </div>
       </div>
