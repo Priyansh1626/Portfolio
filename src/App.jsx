eb00dc9ee774e08da2f5386b2d15af0bcd9e62cf
Priyansh <priyanshpandey2055@gmail.com>
2022-10-10 00:49:09 +0530

    Footer section responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./style/main.css";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-// import BackToTopBtn from "./components/BackToTopBtn";
 import Skill from "./components/Skill";
 
 export default function App() {
@@ -16,7 +14,6 @@ export default function App() {
     <>
       <Home />
       <Navbar />
-      {/* <BackToTopBtn /> */}
       <About />
       <Skill />
       <Projects />
